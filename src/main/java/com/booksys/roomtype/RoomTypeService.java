b7818f47d363f45cd7e150eacdc7a825589813cd
beka toro <bekakikalishvili18@gmail.com>
2024-03-12 15:09:05 +0100

    Merge remote-tracking branch 'github/develop' into develop

--- src/main/java/com/booksys/roomtype/RoomTypeService.java
+++ src/main/java/com/booksys/roomtype/RoomTypeService.java
@@ -1,4 +1,11 @@
 package com.booksys.roomtype;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface RoomTypeService {
+    void save(RoomType roomType);
+    List<RoomType>findbyName(String name);
+    RoomType findbyId(UUID id);
+    List<RoomType> deleteByid(UUID id);
 }
--- src/main/java/com/booksys/roomtype/RoomTypeServiceImpl.java
+++ src/main/java/com/booksys/roomtype/RoomTypeServiceImpl.java
@@ -1,4 +1,36 @@
 package com.booksys.roomtype;
 
-public class RoomTypeServiceImpl {
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class RoomTypeServiceImpl implements RoomTypeService {
+
+    private final RoomTypeRepository roomTypeRepository;
+
+    public RoomTypeServiceImpl(RoomTypeRepository roomTypeRepository) {
+        this.roomTypeRepository = roomTypeRepository;
+    }
+
+    @Override
+    public void save(RoomType roomType) {
+        roomTypeRepository.save(roomType);
+    }
+
+    @Override
+    public List<RoomType> findbyName(String name) {
+        return roomTypeRepository.findByName(name);
+    }
+
+    @Override
+    public RoomType findbyId(UUID id) {
+        return (RoomType) roomTypeRepository.findBytypeId(id).orElseThrow();
+    }
+
+    @Override
+    public List<RoomType> deleteByid(UUID id) {
+        return null;
+    }
 }
