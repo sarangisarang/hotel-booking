106bdb22b7b0f407cb47b190c7273e704087c27a
beka toro <bekakikalishvili18@gmail.com>
2024-03-13 12:41:29 +0100

    Merge remote-tracking branch 'github/develop' into develop

--- src/main/java/com/booksys/staff/StaffRepository.java
+++ src/main/java/com/booksys/staff/StaffRepository.java
@@ -1,4 +1,10 @@
 package com.booksys.staff;
 
-public interface StaffRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StaffRepository extends JpaRepository<Staff,String> {
+    List<Staff> findByFirstName(String name);
+
 }
--- src/main/java/com/booksys/staff/StaffService.java
+++ src/main/java/com/booksys/staff/StaffService.java
@@ -1,4 +1,12 @@
 package com.booksys.staff;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface StaffService {
+    Staff save(Staff staff);
+    void deleteStaffById(UUID id);
+    List<Staff> findByName(String name);
+    Staff findAllStaff();
+
 }
