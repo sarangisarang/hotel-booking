844e37e9be27358728ac1df7c57883a06c2b427d
beka toro <bekakikalishvili18@gmail.com>
2024-05-28 21:55:48 +0200

    Merge remote-tracking branch 'github/develop' into develop

--- src/test/java/com/booksys/hotel/GuestServiceTest.java
+++ src/test/java/com/booksys/hotel/GuestServiceTest.java
@@ -1,11 +1,13 @@
 package com.booksys.hotel;
 import com.booksys.guest.Guest;
+import com.booksys.guest.GuestRepository;
 import com.booksys.guest.GuestService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -13,18 +15,22 @@ public class GuestServiceTest {
     @Autowired
     private GuestService guestService;
 
+    @Autowired
+    private GuestRepository guestRepository;
+
     @Test
     public void GiveAllWhenHasALL(){
         Guest guest = new Guest();
-        guest.setGuestID(UUID.randomUUID());
+        UUID guestId = UUID.randomUUID();
+        guest.setGuestID(guestId);
         guest.setAddress("birkenstrase");
-        guest.setEmail("bekakikalishvili@gmail");
+        guest.setEmail("bekakikalishvili@gmail.com");
         guest.setFirstName("beka");
         guest.setLastName("kikalishvili");
         guest.setPhone("4932343232");
         guest.setDatoOfBirth(LocalDate.ofEpochDay(17-11-1985));
-        guestService.save(guest);
-        Guest newguest  = guestService.findById(guest.getGuestID());
+        guestRepository.save(guest);
+        Guest newguest = guestRepository.findById(String.valueOf(guestId));
         Assertions.assertNotNull(newguest);
     }
     @Test
